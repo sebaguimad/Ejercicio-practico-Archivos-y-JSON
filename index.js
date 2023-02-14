6d42bbd921f09dc66a56b08f07122f36b87822e6
Sebastián Aguilera Madariaga <sebaguimad.94@gmail.com>
2023-02-14 19:05:51 -0300

    ejercicio práctico servidor web en node n4

--- index.js
+++ index.js
@@ -12,41 +12,72 @@ app.use(express.urlencoded());
 
 // Para procesar información del post:
 
-app.get("/productos", (req, res) => {
-    fs.readFile("productos.json", "utf8", (error, data) => {
+app.get("/estudiantes", (req, res) => {
+    fs.readFile("estudiantes.json", "utf8", (error, data) => {
         if(error){
-            return res.status(500).json({error:500, message:"Ha ocurrido un error al leer los productos."})
+            return res.status(500).json({error:500, message:"Ha ocurrido un error al leer los estudiantes."})
         }
-        let productos = JSON.parse(data);
-        res.json(productos);
+        let estudiantes = JSON.parse(data);
+        res.json(estudiantes);
     })
 })
 
-app.post("/productos", (req, res) => {
-    let {nombre, existencias, precio, imagen} = req.body;
-    let productoNuevo = {
+
+
+app.post("/estudiantes", (req, res) => {
+    let {nombre, institucion} = req.body;
+    let estudianteNuevo = {
         codigo: uuidv4().slice(0,6),
         nombre,
-        existencias,
-        precio,
-        imagen
+        institucion,
     }
-    fs.readFile("productos.json", "utf8", (error, data) => {
+    fs.readFile("estudiantes.json", "utf8", (error, data) => {
         if(error){
-            return res.status(500).json({error:500, message:"Ha ocurrido un error al leer los productos."})
+            return res.status(500).json({error:500, message:"Ha ocurrido un error al leer los estudiantes."})
         }
-        let productos = JSON.parse(data);
-        productos.productos.push(productoNuevo);
+        let estudiantes = JSON.parse(data);
+        estudiantes.estudiantes.push(estudianteNuevo);
 
-        fs.writeFile("productos.json", JSON.stringify(productos, null, 4), "utf8", (error) => {
+        fs.writeFile("estudiantes.json", JSON.stringify(estudiantes, null, 4), "utf8", (error) => {
             if(error){
-                return res.status(500).json({error:500, message:"Ha ocurrido un error al leer los productos."})
+                return res.status(500).json({error:500, message:"Ha ocurrido un error al leer los estudiantes."})
             }
-            res.status(201).json(productoNuevo);
+            res.status(201).json(estudianteNuevo);
         })
     })
-
 })
+
+
+// Para usarlo el valor :nombre debe ser escrito en la URL como, por ejemplo: /estudiantes/daniel
+app.delete("/estudiantes/:nombre", (req, res) => {
+    const nombre = req.params.nombre;
+    console.log("Deleting student with name:", nombre);
+    fs.readFile("estudiantes.json", "utf8", (error, data) => {
+      if (error) {
+        return res.status(500).json({ error: 500, message: "Ha ocurrido un error al leer los estudiantes." });
+      }
+  
+      const estudiantes = JSON.parse(data);
+      console.log("Current estudiantes array:", estudiantes);
+      const estudianteIndex = estudiantes.estudiantes.findIndex(estudiante => estudiante.nombre === nombre);
+  
+      if (estudianteIndex === -1) {
+        return res.status(404).json({ error: 404, message: "El estudiante no ha sido encontrado." });
+      }
+  
+      estudiantes.estudiantes.splice(estudianteIndex, 1);
+  
+      fs.writeFile("estudiantes.json", JSON.stringify(estudiantes, null, 4), "utf8", (error) => {
+        if (error) {
+          return res.status(500).json({ error: 500, message: "Ha ocurrido un error al escribir los estudiantes." });
+        }
+        res.sendStatus(204);
+      });
+    });
+});
+  
+
+
 
 app.all("*", (req, res) => {
     res.status(404).send({code: 404, message: "Ruta no habilitada"});
